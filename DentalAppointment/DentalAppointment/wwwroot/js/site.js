fa9d2d18ca3bc3b1669019ea393d135bf5ee47a7
Artem <temaprgod8@gmail.com>
2023-10-15 22:56:51 +0300

    Add card desing in Doctors/Index.cshtml

--- DentalAppointment/DentalAppointment/wwwroot/js/site.js
+++ DentalAppointment/DentalAppointment/wwwroot/js/site.js
@@ -1,4 +1,5 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿document.addEventListener("DOMContentLoaded", function () {
+    var buttonHeight = document.getElementById("details-btn").clientHeight;
+    var customCardBody = document.getElementById("custom-card-body");
+    customCardBody.style.height = `${buttonHeight + 10}px`;
+});
